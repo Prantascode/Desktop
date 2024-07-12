acee6927254f8ff7e4a1cfee1c2293108afe82dc
Pranta Saha <prantasaha907@gmail.com>
2024-07-12 22:07:50 +0600

    mycode

---
+++ pranta's java/Arraylist.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        //cars.set(0,"Mazda");
+        //cars.remove(0);
+        //cars.clear();
+        
+        System.out.println(cars/* .get(1)*/);
+    }
+} 
+    
+
